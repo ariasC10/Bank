9c3045308dbbf7904981d5daa72c0516dca50c26
Joel252 <joel.enrique.cruz860@gmail.com>
2023-02-10 21:18:42 -0500

    replaced old conection

--- BankConsoleApp.DataAccess/Db/AppDbContext.cs
+++ BankConsoleApp.DataAccess/Db/AppDbContext.cs
@@ -3,16 +3,77 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BankConsoleApp.DataAccess.Db
 {
-    public class AppDBContext : DbContext
+    public partial class AppDbContext : DbContext
     {
-        DbSet<Account> Accounts { get; set; }
-        DbSet<Transaction> Transactions { get; set; }
-
-        private const string connectionString = "server=database-bank.c1k9xnmjxukl.us-east-1.rds.amazonaws.com; database=bank; user=admin; password=proyectojorge; SslMode=VerifyFull;";
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public AppDbContext()
         {
-            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql("server=database-bank.c1k9xnmjxukl.us-east-1.rds.amazonaws.com;database=bank;user=admin;password=proyectojorge", ServerVersion.Parse("8.0.32-mysql"));
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.AccountNumber).HasName("PRIMARY");
+
+                entity.ToTable("account");
+
+                entity.Property(e => e.AccountNumber)
+                    .ValueGeneratedNever()
+                    .HasColumnName("account_number");
+                entity.Property(e => e.AccountType)
+                    .HasMaxLength(7)
+                    .HasColumnName("account_type");
+                entity.Property(e => e.Balance).HasColumnName("balance");
+                entity.Property(e => e.CreationDate).HasColumnName("creation_date");
+                entity.Property(e => e.Owner)
+                    .HasMaxLength(45)
+                    .HasColumnName("owner");
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+                entity.HasKey(e => e.TransactionNumber).HasName("PRIMARY");
+
+                entity.ToTable("transaction");
+
+                entity.HasIndex(e => e.AccountNumber, "fk_accountnumber_idx");
+
+                entity.Property(e => e.TransactionNumber)
+                    .ValueGeneratedNever()
+                    .HasColumnName("transaction_number");
+                entity.Property(e => e.AccountNumber).HasColumnName("account_number");
+                entity.Property(e => e.CreationDate).HasColumnName("creation_date");
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .HasDefaultValueSql("'N/A'")
+                    .HasColumnName("description");
+                entity.Property(e => e.Mount).HasColumnName("mount");
+
+                entity.HasOne(d => d.AccountNumberNavigation).WithMany(p => p.Transactions)
+                    .HasForeignKey(d => d.AccountNumber)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_accountnumber");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
 
