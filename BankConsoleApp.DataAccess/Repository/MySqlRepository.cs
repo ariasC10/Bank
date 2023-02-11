5160369f35f364ac4aac37c42bc7e2b317b66c22
Joel252 <joel.enrique.cruz860@gmail.com>
2023-02-10 21:49:35 -0500

    update MySqlRepository

---
+++ BankConsoleApp.DataAccess/Repository/MySqlRepository.cs
@@ -0,0 +1,36 @@
+﻿using BankConsoleApp.Core.Models;
+using BankConsoleApp.DataAccess.Db;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsoleApp.DataAccess.Repository
+{
+    internal class MySqlRepository : IRepository<Account>
+    {
+        private readonly DbContext _dbContext;
+
+        public MySqlRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<bool> Add(Account model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Account model, double mount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
