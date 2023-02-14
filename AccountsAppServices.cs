1defe6121b365e9304f9266b5a994af40cf1a756
ariasC10 <107955824+ariasC10@users.noreply.github.com>
2023-02-14 17:32:44 -0600

    xd

---
+++ AccountsAppServices.cs
@@ -0,0 +1,132 @@
+﻿using BankConsoleApp.Core.Models;
+using BankConsoleApp.DataAccess.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankConsoleApp.Service.Accounts;
+public class AccountsAppServices : IAccountsAppServices
+{
+    private readonly IRepository<Account> _repository;
+    private Account CurrentAccount;
+
+    public AccountsAppServices(IRepository<Account> accountRepository)
+    {
+        _repository = accountRepository;
+    }
+
+    public async void AddAccount(string owner, string accountType)
+    {
+        // Generar el número de cuenta a partir de una seed -> private uint generateAccountNumber()
+        // Asignar fecha actual -> DateOnly.FromDateTime(DateTime.Now)
+        // Balance inicial 0
+
+        // Crear una nueva instancia de Account con los detalles proporcionados
+
+        var newAccount = new Account
+        {
+            Owner = owner,
+            AccountType = accountType,
+            CreationDate = DateOnly.FromDateTime(DateTime.Now),
+            Balance = 0
+        };
+
+        await _repository.Insert(newAccount);
+
+        CurrentAccount = newAccount;
+    }
+
+    private bool AuthorizeTransaction(float amount, Account account)
+    {
+        if (amount >= 0)
+        {
+            return account.Balance >= amount;
+        }
+        else
+        {
+            return account.AccountType == "credit" && Math.Abs(amount) <= account.Balance;
+        }
+    }
+
+
+    public void DoTransaction(float mount, string description)
+    {
+        // si CurrentAccount es de tipo saving, no pede hacer depositos de menos de 100
+        // Verificar que tiene saldo suficiente si el mount es negativo -> private bool authorizeTransaction
+
+        // formato de las transacciones :
+        //  TransactionNumber es igual a -> AccountCurrent.Transactions.Count + 1
+        //  Asignar fecha actual -> DateOnly.FromDateTime(DateTime.Now)
+        //  Se le asigna el numero de cuenta de CurrentAccount
+        //  No agregar nada a AccountNumberNavigation
+
+        // realizar update del modelo
+
+        if (CurrentAccount == null)
+        {
+            throw new NullReferenceException("Current account has not been selected");
+        }
+
+        if (CurrentAccount.AccountType == "saving" && mount < 100 && description.ToLower() != "interest")
+        {
+            throw new ArgumentOutOfRangeException("Saving accounts can only make deposits of 100 or more");
+        }
+        //
+        if (mount < 0 && !AuthorizeTransaction(-mount, CurrentAccount))
+        {
+            throw new ArgumentException("Insufficient balance");
+        }
+
+        var newTransaction = new Transaction
+        {
+            Mount = mount,
+            Description = description,
+            CreationDate = DateOnly.FromDateTime(DateTime.Now),
+            AccountNumber = CurrentAccount.AccountNumber
+        };
+
+        CurrentAccount.Transactions.Add(newTransaction);
+
+        _repository.Update(CurrentAccount);
+    }
+
+    public void GetBalanace(int accountNumber)
+    {
+        var account = _repository.GetById((uint)accountNumber);
+
+        if (account == null)
+        {
+            Console.WriteLine("Account not found.");
+            return;
+        }
+
+        CurrentAccount = account;
+
+        float balance = 0f;
+        foreach (var transaction in CurrentAccount.Transactions)
+        {
+            balance += transaction.Mount;
+        }
+
+        Console.WriteLine($"Account balance: {balance}");
+    }
+
+    public void SelectAccount(uint id)
+    {
+        var account = _repository.GetById(id);
+
+        if (account == null)
+        {
+            Console.WriteLine("Account not found.");
+            return;
+        }
+
+        CurrentAccount = account;
+    }
+}
